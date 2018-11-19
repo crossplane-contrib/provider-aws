7f69a5409ba34057aa2244a1f45f19ef1972e73f
Bassam Tabbara <bassam@upbound.io>
2018-11-19 12:53:07 -0800

    rename to crossplane
    make build and make test now work.

--- cmd/crossplane/main.go
+++ cmd/crossplane/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The Conductor Authors.
+Copyright 2018 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,8 +20,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/upbound/conductor/pkg/apis"
-	"github.com/upbound/conductor/pkg/controller"
+	"github.com/crossplaneio/crossplane/pkg/apis"
+	"github.com/crossplaneio/crossplane/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
