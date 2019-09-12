7d30ad8b67c64c362538e9fd6544ebb27faa0b6f
Jared Watts <jbw976@gmail.com>
2019-09-12 15:55:37 -0700

    add doc gen tags for new networking groups

--- aws/apis/network/v1alpha2/doc.go
+++ aws/apis/network/v1alpha2/doc.go
@@ -14,4 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package v1alpha2 contains managed resources for AWS network services such as
+// VPC and Subnet.
+// +kubebuilder:object:generate=true
+// +groupName=network.aws.crossplane.io
+// +versionName=v1alpha2
 package v1alpha2
