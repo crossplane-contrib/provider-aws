96111f909c0583c9f3f7dd9b48e400969b3cfc04
soorena776 <javad@upbound.io>
2019-09-27 12:03:53 -0700

    addressing issue 9
    Signed-off-by: soorena776 <javad@upbound.io>

---
+++ pkg/controller/cache/claim.go
@@ -0,0 +1,149 @@
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cache
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/crossplaneio/stack-aws/apis/cache/v1alpha2"
+
+	runtimev1alpha1 "github.com/crossplaneio/crossplane-runtime/apis/core/v1alpha1"
+	"github.com/crossplaneio/crossplane-runtime/pkg/resource"
+	cachev1alpha1 "github.com/crossplaneio/crossplane/apis/cache/v1alpha1"
+)
+
+// ReplicationGroupClaimController is responsible for adding the ReplicationGroup
+// claim controller and its corresponding reconciler to the manager with any runtime configuration.
+type ReplicationGroupClaimController struct{}
+
+// SetupWithManager adds a controller that reconciles RedisCluster resource claims.
+func (c *ReplicationGroupClaimController) SetupWithManager(mgr ctrl.Manager) error {
+	name := strings.ToLower(fmt.Sprintf("%s.%s.%s",
+		cachev1alpha1.RedisClusterKind,
+		v1alpha2.ReplicationGroupKind,
+		v1alpha2.Group))
+
+	r := resource.NewClaimReconciler(mgr,
+		resource.ClaimKind(cachev1alpha1.RedisClusterGroupVersionKind),
+		resource.ClassKinds{
+			Portable:    cachev1alpha1.RedisClusterClassGroupVersionKind,
+			NonPortable: v1alpha2.ReplicationGroupClassGroupVersionKind,
+		},
+		resource.ManagedKind(v1alpha2.ReplicationGroupGroupVersionKind),
+		resource.WithManagedBinder(resource.NewAPIManagedStatusBinder(mgr.GetClient())),
+		resource.WithManagedFinalizer(resource.NewAPIManagedStatusUnbinder(mgr.GetClient())),
+		resource.WithManagedConfigurators(
+			resource.ManagedConfiguratorFn(ConfigureReplicationGroup),
+			resource.NewObjectMetaConfigurator(mgr.GetScheme()),
+		))
+
+	p := resource.NewPredicates(resource.AnyOf(
+		resource.HasManagedResourceReferenceKind(resource.ManagedKind(v1alpha2.ReplicationGroupGroupVersionKind)),
+		resource.IsManagedKind(resource.ManagedKind(v1alpha2.ReplicationGroupGroupVersionKind), mgr.GetScheme()),
+		resource.HasIndirectClassReferenceKind(mgr.GetClient(), mgr.GetScheme(), resource.ClassKinds{
+			Portable:    cachev1alpha1.RedisClusterClassGroupVersionKind,
+			NonPortable: v1alpha2.ReplicationGroupClassGroupVersionKind,
+		})))
+
+	return ctrl.NewControllerManagedBy(mgr).
+		Named(name).
+		Watches(&source.Kind{Type: &v1alpha2.ReplicationGroup{}}, &resource.EnqueueRequestForClaim{}).
+		For(&cachev1alpha1.RedisCluster{}).
+		WithEventFilter(p).
+		Complete(r)
+}
+
+// ConfigureReplicationGroup configures the supplied resource (presumed
+// to be a ReplicationGroup) using the supplied resource claim (presumed
+// to be a RedisCluster) and resource class.
+func ConfigureReplicationGroup(_ context.Context, cm resource.Claim, cs resource.NonPortableClass, mg resource.Managed) error {
+	rc, cmok := cm.(*cachev1alpha1.RedisCluster)
+	if !cmok {
+		return errors.Errorf("expected resource claim %s to be %s", cm.GetName(), cachev1alpha1.RedisClusterGroupVersionKind)
+	}
+
+	rs, csok := cs.(*v1alpha2.ReplicationGroupClass)
+	if !csok {
+		return errors.Errorf("expected resource class %s to be %s", cs.GetName(), v1alpha2.ReplicationGroupClassGroupVersionKind)
+	}
+
+	i, mgok := mg.(*v1alpha2.ReplicationGroup)
+	if !mgok {
+		return errors.Errorf("expected managed resource %s to be %s", mg.GetName(), v1alpha2.ReplicationGroupGroupVersionKind)
+	}
+
+	spec := &v1alpha2.ReplicationGroupSpec{
+		ResourceSpec: runtimev1alpha1.ResourceSpec{
+			ReclaimPolicy: runtimev1alpha1.ReclaimRetain,
+		},
+		ReplicationGroupParameters: rs.SpecTemplate.ReplicationGroupParameters,
+	}
+
+	if err := resolveAWSClassInstanceValues(spec, rc); err != nil {
+		return errors.Wrap(err, "cannot resolve AWS class instance values")
+	}
+
+	spec.WriteConnectionSecretToReference = corev1.LocalObjectReference{Name: string(cm.GetUID())}
+	spec.ProviderReference = rs.SpecTemplate.ProviderReference
+	spec.ReclaimPolicy = rs.SpecTemplate.ReclaimPolicy
+
+	i.Spec = *spec
+
+	return nil
+}
+
+func resolveAWSClassInstanceValues(spec *v1alpha2.ReplicationGroupSpec, rc *cachev1alpha1.RedisCluster) error {
+	var err error
+	switch {
+	case spec.EngineVersion == "" && rc.Spec.EngineVersion == "":
+	// Neither the claim nor its class specified a version. Let AWS pick.
+
+	case spec.EngineVersion == "" && rc.Spec.EngineVersion != "":
+		// Only the claim specified a version. Use the latest supported patch
+		// version for said claim (minor) version.
+		spec.EngineVersion, err = latestSupportedPatchVersion(rc.Spec.EngineVersion)
+
+	case spec.EngineVersion != "" && rc.Spec.EngineVersion == "":
+		// Only the class specified a version. Use it.
+
+	case !strings.HasPrefix(spec.EngineVersion, rc.Spec.EngineVersion+"."):
+		// Both the claim and its class specified a version, but the class
+		// version is not a patch of the claim version.
+		err = errors.Errorf("class version %s is not a patch of claim version %s", spec.EngineVersion, rc.Spec.EngineVersion)
+
+	default:
+		// Both the claim and its class specified a version, and the class
+		// version is a patch of the claim version. Use the class version.
+	}
+
+	return errors.Wrap(err, "cannot resolve class claim values")
+}
+
+func latestSupportedPatchVersion(minorVersion string) (string, error) {
+	p := v1alpha2.LatestSupportedPatchVersion[v1alpha2.MinorVersion(minorVersion)]
+	if p == v1alpha2.UnsupportedVersion {
+		return "", errors.Errorf("minor version %s is not currently supported", minorVersion)
+	}
+	return string(p), nil
+}
