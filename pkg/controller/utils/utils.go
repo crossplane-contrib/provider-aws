980c503f99fa945e090f6e6984221e8355e9476a
Nic Cope <negz@rk0n.org>
2019-10-22 20:59:04 -0700

    Update controller utils for cluster scoping
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- pkg/controller/utils/utils.go
+++ pkg/controller/utils/utils.go
@@ -1,3 +1,22 @@
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// NOTE(negz): Please do not add methods to this package. See below issue.
+// https://github.com/crossplaneio/crossplane-runtime/issues/1
+
 package utils
 
 import (
@@ -24,7 +43,7 @@ func RetrieveAwsConfigFromProvider(ctx context.Context, client client.Reader, pr
 	}
 
 	secret := &corev1.Secret{}
-	n = types.NamespacedName{Namespace: p.GetNamespace(), Name: p.Spec.Secret.Name}
+	n = types.NamespacedName{Namespace: p.Spec.Secret.Namespace, Name: p.Spec.Secret.Name}
 	err := client.Get(ctx, n, secret)
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot get provider secret %s", n)
