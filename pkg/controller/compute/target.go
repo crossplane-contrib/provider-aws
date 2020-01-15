db8db3722b05cec4f6c1e31a85e3ca2b54f7bfd5
hasheddan <georgedanielmangum@gmail.com>
2020-01-15 17:40:50 -0600

    k8s target: add kubernetes target controller
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ pkg/controller/compute/target.go
@@ -0,0 +1,50 @@
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package compute
+
+import (
+	"fmt"
+	"strings"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplaneio/crossplane-runtime/pkg/reconciler/target"
+	"github.com/crossplaneio/crossplane-runtime/pkg/resource"
+	"github.com/crossplaneio/crossplane/apis/workload/v1alpha1"
+
+	"github.com/crossplaneio/stack-aws/apis/compute/v1alpha3"
+)
+
+// EKSClusterTargetController is responsible for adding the EKSCluster target
+// controller and its corresponding reconciler to the manager with any runtime configuration.
+type EKSClusterTargetController struct{}
+
+// SetupWithManager adds a controller that propagates EKSCluster connection
+// secrets to the connection secrets of their targets.
+func (c *EKSClusterTargetController) SetupWithManager(mgr ctrl.Manager) error {
+	p := resource.NewPredicates(resource.HasManagedResourceReferenceKind(resource.ManagedKind(v1alpha3.EKSClusterGroupVersionKind)))
+
+	r := target.NewReconciler(mgr,
+		resource.TargetKind(v1alpha1.KubernetesTargetGroupVersionKind),
+		resource.ManagedKind(v1alpha3.EKSClusterGroupVersionKind))
+
+	return ctrl.NewControllerManagedBy(mgr).
+		Named(strings.ToLower(fmt.Sprintf("kubernetestarget.%s.%s", v1alpha3.EKSClusterKind, v1alpha3.Group))).
+		For(&v1alpha1.KubernetesTarget{}).
+		WithEventFilter(p).
+		Complete(r)
+}
