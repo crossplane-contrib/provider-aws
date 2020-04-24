e90cf7d022d3b6fd395170202633b9ccf8475e63
ajaykangare <ajaykangare1993@gmail.com>
2020-04-24 12:53:36 +0530

    added code for register resource

--- pkg/clients/certificatemanager/certificate.go
+++ pkg/clients/certificatemanager/certificate.go
@@ -3,6 +3,7 @@ package certificatemanager
 import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/acm"
+
 	"github.com/crossplane/provider-aws/apis/certificatemanager/v1alpha1"
 )
 
