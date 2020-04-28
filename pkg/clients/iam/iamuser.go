6988923ded7895723a3bfff523dd1be02fc57624
sahil-lakhwani <sahilakhwani@gmail.com>
2020-04-28 20:47:23 +0530

    iamuser: remove group membership
    Signed-off-by: sahil-lakhwani <sahilakhwani@gmail.com>

--- pkg/clients/iam/iamuser.go
+++ pkg/clients/iam/iamuser.go
@@ -2,12 +2,10 @@ package iam
 
 import (
 	"context"
-	"sort"
 
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 
 	"github.com/crossplane/provider-aws/apis/identity/v1alpha1"
-	aws "github.com/crossplane/provider-aws/pkg/clients"
 	awsclients "github.com/crossplane/provider-aws/pkg/clients"
 )
 
@@ -17,9 +15,6 @@ type UserClient interface {
 	CreateUserRequest(*iam.CreateUserInput) iam.CreateUserRequest
 	UpdateUserRequest(*iam.UpdateUserInput) iam.UpdateUserRequest
 	DeleteUserRequest(*iam.DeleteUserInput) iam.DeleteUserRequest
-	ListGroupsForUserRequest(*iam.ListGroupsForUserInput) iam.ListGroupsForUserRequest
-	AddUserToGroupRequest(*iam.AddUserToGroupInput) iam.AddUserToGroupRequest
-	RemoveUserFromGroupRequest(*iam.RemoveUserFromGroupInput) iam.RemoveUserFromGroupRequest
 }
 
 // NewUserClient returns a new client using AWS credentials as JSON encoded data.
@@ -43,30 +38,9 @@ func LateInitializeUser(in *v1alpha1.IAMUserParameters, user *iam.User) {
 		in.PermissionsBoundary = awsclients.LateInitializeStringPtr(in.PermissionsBoundary, user.PermissionsBoundary.PermissionsBoundaryArn)
 	}
 
-	for _, tag := range user.Tags {
-		in.Tags = append(in.Tags, v1alpha1.Tag{Key: *tag.Key, Value: *tag.Value})
-	}
-}
-
-// CompareGroups compares list of groups specified for v1alpha1.User and
-// list of actual groups for iam.User
-func CompareGroups(p v1alpha1.IAMUserParameters, groups []iam.Group) bool {
-	if len(p.GroupList) != len(groups) {
-		return false
-	}
-
-	sort.Slice(p.GroupList, func(i, j int) bool {
-		return p.GroupList[i] < p.GroupList[j]
-	})
-	sort.Slice(groups, func(i, j int) bool {
-		return aws.StringValue(groups[i].GroupName) < aws.StringValue(groups[j].GroupName)
-	})
-
-	for i, val := range groups {
-		if p.GroupList[i] != aws.StringValue(val.GroupName) {
-			return false
+	if in.Tags == nil && user.Tags != nil {
+		for _, tag := range user.Tags {
+			in.Tags = append(in.Tags, v1alpha1.Tag{Key: *tag.Key, Value: *tag.Value})
 		}
 	}
-
-	return true
 }
