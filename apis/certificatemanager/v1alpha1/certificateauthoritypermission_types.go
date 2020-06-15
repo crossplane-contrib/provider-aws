13d0c623a9ef41ae6e4d9f213631806d4ee3595a
ajaykangare <ajaykangare1993@gmail.com>
2020-06-15 19:52:59 +0530

    Added controller for CA permission
    Signed-off-by: ajaykangare <ajaykangare1993@gmail.com>

---
+++ apis/certificatemanager/v1alpha1/certificateauthoritypermission_types.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// CertificateAuthorityPermissionSpec defines the desired state of CertificateAuthorityPermission
+type CertificateAuthorityPermissionSpec struct {
+	runtimev1alpha1.ResourceSpec `json:",inline"`
+	ForProvider                  CertificateAuthorityPermissionParameters `json:"forProvider"`
+}
+
+// An CertificateAuthorityPermissionStatus represents the observed state of an Certificate Authority Permission manager.
+type CertificateAuthorityPermissionStatus struct {
+	runtimev1alpha1.ResourceStatus `json:",inline"`
+}
+
+// CertificateAuthorityPermissionParameters defines the desired state of an AWS CertificateAuthority.
+type CertificateAuthorityPermissionParameters struct {
+
+	// The Amazon Resource Name (ARN) of the private certificate authority (CA)that will be used to issue the certificate.
+	// +immutable
+	CertificateAuthorityARN *string `json:"certificateAuthorityArn,omitempty"`
+
+	// CertificateAuthorityArnRef references an CertificateAuthority to retrieve its Arn
+	// +optional
+	// +immutable
+	CertificateAuthorityARNRef *runtimev1alpha1.Reference `json:"certificateAuthorityArnRef,omitempty"`
+
+	// CertificateAuthorityArnSelector selects a reference to an CertificateAuthority to retrieve its Arn
+	// +optional
+	// +immutable
+	CertificateAuthorityARNSelector *runtimev1alpha1.Selector `json:"certificateAuthorityArnSelector,omitempty"`
+
+	// The actions that the specified AWS service principal can use.
+	// +optional
+	// +immutable
+	Actions []string `json:"actions,omitempty"`
+
+	// The AWS Service or identity
+	// +optional
+	// +immutable
+	Principal *string `json:"principal,omitempty"`
+
+	// Calling Account ID
+	// +optional
+	// +immutable
+	SourceAccount *string `json:"sourceAccount,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// CertificateAuthorityPermission is a managed resource that represents an AWS CertificateAuthorityPermission Manager.
+// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
+// +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+type CertificateAuthorityPermission struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CertificateAuthorityPermissionSpec   `json:"spec,omitempty"`
+	Status CertificateAuthorityPermissionStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// CertificateAuthorityPermissionList contains a list of CertificateAuthorityPermission
+type CertificateAuthorityPermissionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CertificateAuthorityPermission `json:"items"`
+}
--- apis/certificatemanager/v1alpha1/zz_generated.managed.go
+++ apis/certificatemanager/v1alpha1/zz_generated.managed.go
@@ -92,3 +92,73 @@ func (mg *CertificateAuthority) SetReclaimPolicy(r runtimev1alpha1.ReclaimPolicy
 func (mg *CertificateAuthority) SetWriteConnectionSecretToReference(r *runtimev1alpha1.SecretReference) {
 	mg.Spec.WriteConnectionSecretToReference = r
 }
+
+// GetBindingPhase of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetBindingPhase() runtimev1alpha1.BindingPhase {
+	return mg.Status.GetBindingPhase()
+}
+
+// GetClaimReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetClaimReference() *corev1.ObjectReference {
+	return mg.Spec.ClaimReference
+}
+
+// GetClassReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetClassReference() *corev1.ObjectReference {
+	return mg.Spec.ClassReference
+}
+
+// GetCondition of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetCondition(ct runtimev1alpha1.ConditionType) runtimev1alpha1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetProviderReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetProviderReference() *corev1.ObjectReference {
+	return mg.Spec.ProviderReference
+}
+
+// GetReclaimPolicy of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetReclaimPolicy() runtimev1alpha1.ReclaimPolicy {
+	return mg.Spec.ReclaimPolicy
+}
+
+// GetWriteConnectionSecretToReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) GetWriteConnectionSecretToReference() *runtimev1alpha1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetBindingPhase of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetBindingPhase(p runtimev1alpha1.BindingPhase) {
+	mg.Status.SetBindingPhase(p)
+}
+
+// SetClaimReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetClaimReference(r *corev1.ObjectReference) {
+	mg.Spec.ClaimReference = r
+}
+
+// SetClassReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetClassReference(r *corev1.ObjectReference) {
+	mg.Spec.ClassReference = r
+}
+
+// SetConditions of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetConditions(c ...runtimev1alpha1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetProviderReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetProviderReference(r *corev1.ObjectReference) {
+	mg.Spec.ProviderReference = r
+}
+
+// SetReclaimPolicy of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetReclaimPolicy(r runtimev1alpha1.ReclaimPolicy) {
+	mg.Spec.ReclaimPolicy = r
+}
+
+// SetWriteConnectionSecretToReference of this CertificateAuthorityPermission.
+func (mg *CertificateAuthorityPermission) SetWriteConnectionSecretToReference(r *runtimev1alpha1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
