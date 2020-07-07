4fc9df5385535abf43ad5f3f9bdbca66db174f30
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-07-07 18:00:13 +0300

    resource record set: connect function has the standard implementation now
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- pkg/clients/resourcerecordset/resourcerecordset.go
+++ pkg/clients/resourcerecordset/resourcerecordset.go
@@ -60,8 +60,12 @@ func IsNotFound(err error) bool {
 }
 
 // NewClient creates new AWS client with provided AWS Configuration/Credentials
-func NewClient(config *aws.Config) Client {
-	return route53.New(*config)
+func NewClient(ctx context.Context, credentials []byte, region string, auth awsclients.AuthMethod) (Client, error) {
+	cfg, err := auth(ctx, credentials, awsclients.DefaultSection, region)
+	if cfg == nil {
+		return nil, err
+	}
+	return route53.New(*cfg), err
 }
 
 // GetResourceRecordSet returns recordSet if present or err
