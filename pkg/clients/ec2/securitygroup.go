d361ad8f022d445df242ed47c477142588646308
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-07-29 00:17:22 +0300

    security group: fixes for comparison method to return true when there is no update to be made
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- pkg/clients/ec2/securitygroup.go
+++ pkg/clients/ec2/securitygroup.go
@@ -3,6 +3,7 @@ package ec2
 import (
 	"context"
 	"encoding/json"
+	"strings"
 
 	awsgo "github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/awserr"
@@ -174,12 +175,13 @@ func LateInitializeSG(in *v1beta1.SecurityGroupParameters, sg *ec2.SecurityGroup
 	in.GroupName = awsclients.LateInitializeString(in.GroupName, sg.GroupName)
 	in.VPCID = awsclients.LateInitializeStringPtr(in.VPCID, sg.VpcId)
 
-	if len(in.Egress) == 0 && len(sg.IpPermissionsEgress) != 0 {
-		in.Egress = GenerateIPPermissions(sg.IpPermissionsEgress)
+	// If there is a mismatch in lengths, then it's a sign for desire change
+	// that should be handled via add or remove.
+	if len(in.Egress) == len(sg.IpPermissionsEgress) {
+		in.Egress = LateInitializeIPPermissions(in.Egress, sg.IpPermissionsEgress)
 	}
-
-	if len(in.Ingress) == 0 && len(sg.IpPermissions) != 0 {
-		in.Ingress = GenerateIPPermissions(sg.IpPermissions)
+	if len(in.Ingress) == len(sg.IpPermissions) {
+		in.Ingress = LateInitializeIPPermissions(in.Ingress, sg.IpPermissions)
 	}
 
 	if len(in.Tags) == 0 && len(sg.Tags) != 0 {
@@ -187,15 +189,93 @@ func LateInitializeSG(in *v1beta1.SecurityGroupParameters, sg *ec2.SecurityGroup
 	}
 }
 
+// LateInitializeIPPermissions returns an array of []v1beta1.IPPermission whose
+// empty optional fields are filled with what's observed in []ec2.IpPermission.
+//
+// Note that since there is no unique identifier for each IPPermission, its order
+// is assumed to be stable and used indexes are used as identifier.
+func LateInitializeIPPermissions(spec []v1beta1.IPPermission, o []ec2.IpPermission) []v1beta1.IPPermission { // nolint:gocyclo
+	if len(spec) < len(o) {
+		return spec
+	}
+	for i := range o {
+		spec[i].FromPort = awsclients.LateInitializeInt64Ptr(spec[i].FromPort, o[i].FromPort)
+		spec[i].ToPort = awsclients.LateInitializeInt64Ptr(spec[i].FromPort, o[i].ToPort)
+		spec[i].IPProtocol = awsclients.LateInitializeString(spec[i].IPProtocol, o[i].IpProtocol)
+
+		for j := range o[i].IpRanges {
+			if len(spec[i].IPRanges) == j {
+				spec[i].IPRanges = append(spec[i].IPRanges, v1beta1.IPRange{})
+			}
+			spec[i].IPRanges[j].Description = awsclients.LateInitializeStringPtr(
+				spec[i].IPRanges[j].Description,
+				o[i].IpRanges[j].Description,
+			)
+		}
+		for j := range o[i].Ipv6Ranges {
+			if len(spec[i].IPv6Ranges) == j {
+				spec[i].IPv6Ranges = append(spec[i].IPv6Ranges, v1beta1.IPv6Range{})
+			}
+			spec[i].IPv6Ranges[j].Description = awsclients.LateInitializeStringPtr(
+				spec[i].IPv6Ranges[j].Description,
+				o[i].Ipv6Ranges[j].Description,
+			)
+		}
+		for j := range o[i].PrefixListIds {
+			if len(spec[i].PrefixListIDs) == j {
+				spec[i].PrefixListIDs = append(spec[i].PrefixListIDs, v1beta1.PrefixListID{})
+			}
+			spec[i].PrefixListIDs[j].Description = awsclients.LateInitializeStringPtr(
+				spec[i].PrefixListIDs[j].Description,
+				o[i].PrefixListIds[j].Description,
+			)
+		}
+		for j := range o[i].UserIdGroupPairs {
+			if len(spec[i].UserIDGroupPairs) == j {
+				spec[i].UserIDGroupPairs = append(spec[i].UserIDGroupPairs, v1beta1.UserIDGroupPair{})
+			}
+			spec[i].UserIDGroupPairs[j].Description = awsclients.LateInitializeStringPtr(
+				spec[i].UserIDGroupPairs[j].Description,
+				o[i].UserIdGroupPairs[j].Description,
+			)
+			spec[i].UserIDGroupPairs[j].GroupID = awsclients.LateInitializeStringPtr(
+				spec[i].UserIDGroupPairs[j].GroupID,
+				o[i].UserIdGroupPairs[j].GroupId,
+			)
+			spec[i].UserIDGroupPairs[j].GroupName = awsclients.LateInitializeStringPtr(
+				spec[i].UserIDGroupPairs[j].GroupName,
+				o[i].UserIdGroupPairs[j].GroupName,
+			)
+			spec[i].UserIDGroupPairs[j].UserID = awsclients.LateInitializeStringPtr(
+				spec[i].UserIDGroupPairs[j].UserID,
+				o[i].UserIdGroupPairs[j].UserId,
+			)
+			spec[i].UserIDGroupPairs[j].VPCID = awsclients.LateInitializeStringPtr(
+				spec[i].UserIDGroupPairs[j].VPCID,
+				o[i].UserIdGroupPairs[j].VpcId,
+			)
+			spec[i].UserIDGroupPairs[j].VPCPeeringConnectionID = awsclients.LateInitializeStringPtr(
+				spec[i].UserIDGroupPairs[j].VPCPeeringConnectionID,
+				o[i].UserIdGroupPairs[j].VpcPeeringConnectionId,
+			)
+		}
+	}
+	return spec
+}
+
 // CreateSGPatch creates a *v1beta1.SecurityGroupParameters that has only the changed
 // values between the target *v1beta1.SecurityGroupParameters and the current
 // *ec2.SecurityGroup
-func CreateSGPatch(in ec2.SecurityGroup, target v1beta1.SecurityGroupParameters) (*v1beta1.SecurityGroupParameters, error) {
-	currentParams := &v1beta1.SecurityGroupParameters{}
-
+func CreateSGPatch(in ec2.SecurityGroup, target v1beta1.SecurityGroupParameters) (*v1beta1.SecurityGroupParameters, error) { // nolint:gocyclo
+	// We initialize these arrays with the correct length so that late-init
+	// iteration works; workaround for using empty SecurityGroupParameters with
+	// late-init.
+	currentParams := &v1beta1.SecurityGroupParameters{
+		Ingress: make([]v1beta1.IPPermission, len(in.IpPermissions)),
+		Egress:  make([]v1beta1.IPPermission, len(in.IpPermissionsEgress)),
+	}
 	v1beta1.SortTags(target.Tags, in.Tags)
 	LateInitializeSG(currentParams, &in)
-
 	// NOTE(muvaf): Sending -1 as FromPort or ToPort is valid but the returned
 	// object does not have that value. So, in case we have sent -1, we assume
 	// that the returned value is also -1 in case if it's nil.
@@ -211,6 +291,15 @@ func CreateSGPatch(in ec2.SecurityGroup, target v1beta1.SecurityGroupParameters)
 		}
 		if awsgo.Int64Value(spec.ToPort) == mOne {
 			currentParams.Egress[i].ToPort = awsclients.LateInitializeInt64Ptr(currentParams.Egress[i].ToPort, &mOne)
+		}
+	}
+	// Same happens with VPCID in egress user group id pairs. The value of that
+	// field is not returned from AWS.
+	for i, ingress := range currentParams.Ingress {
+		for j, pair := range ingress.UserIDGroupPairs {
+			if awsclients.StringValue(pair.VPCID) == "" && len(target.Ingress) > i && len(target.Ingress[i].UserIDGroupPairs) > j {
+				currentParams.Ingress[i].UserIDGroupPairs[j].VPCID = target.Ingress[i].UserIDGroupPairs[j].VPCID
+			}
 		}
 	}
 
@@ -232,5 +321,34 @@ func IsSGUpToDate(p v1beta1.SecurityGroupParameters, sg ec2.SecurityGroup) (bool
 	if err != nil {
 		return false, err
 	}
-	return cmp.Equal(&v1beta1.SecurityGroupParameters{}, patch, cmpopts.IgnoreTypes(&v1alpha1.Reference{}, &v1alpha1.Selector{})), nil
+	return cmp.Equal(&v1beta1.SecurityGroupParameters{}, patch,
+		cmpopts.IgnoreTypes(&v1alpha1.Reference{}, &v1alpha1.Selector{}),
+		InsensitiveCases()), nil
+}
+
+// TODO(muvaf): We needed this for IPProtocol field; even if you send "TCP", AWS
+// returns "tcp". However, this cmp.Option is probably useful for other providers,
+// too. Consider making it part of crossplane-runtime.
+
+// InsensitiveCases ignores the case sensitivity for string and *string types.
+func InsensitiveCases() cmp.Option {
+	return cmp.FilterValues(func(x, y interface{}) bool {
+		return true
+	}, cmp.Comparer(func(x, y interface{}) bool {
+		a := ""
+		b := ""
+		switch v := x.(type) {
+		case string:
+			a = v
+		case *string:
+			a = awsclients.StringValue(v)
+		}
+		switch v := y.(type) {
+		case string:
+			b = v
+		case *string:
+			b = awsclients.StringValue(v)
+		}
+		return strings.EqualFold(a, b)
+	}))
 }
