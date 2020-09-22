ad476c5eeb416c0085dba28977b93a1c1cf7d7a3
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-09-22 18:25:37 +0300

    remove classes

--- apis/database/v1beta1/rdsinstance_types.go
+++ apis/database/v1beta1/rdsinstance_types.go
@@ -1114,36 +1114,3 @@ type RDSInstanceList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []RDSInstance `json:"items"`
 }
-
-// An RDSInstanceClassSpecTemplate is a template for the spec of a dynamically
-// provisioned RDSInstance.
-type RDSInstanceClassSpecTemplate struct {
-	runtimev1alpha1.ClassSpecTemplate `json:",inline"`
-	ForProvider                       RDSInstanceParameters `json:"forProvider"`
-}
-
-// +kubebuilder:object:root=true
-
-// An RDSInstanceClass is a resource class. It defines the desired spec of
-// resource claims that use it to dynamically provision a managed resource.
-// +kubebuilder:printcolumn:name="PROVIDER-REF",type="string",JSONPath=".specTemplate.providerRef.name"
-// +kubebuilder:printcolumn:name="RECLAIM-POLICY",type="string",JSONPath=".specTemplate.reclaimPolicy"
-// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
-// +kubebuilder:resource:scope=Cluster,categories={crossplane,class,aws}
-type RDSInstanceClass struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	// SpecTemplate is a template for the spec of a dynamically provisioned
-	// RDSInstance.
-	SpecTemplate RDSInstanceClassSpecTemplate `json:"specTemplate"`
-}
-
-// +kubebuilder:object:root=true
-
-// RDSInstanceClassList contains a list of cloud memorystore resource classes.
-type RDSInstanceClassList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []RDSInstanceClass `json:"items"`
-}
--- apis/database/v1beta1/register.go
+++ apis/database/v1beta1/register.go
@@ -45,14 +45,6 @@ var (
 	RDSInstanceGroupVersionKind = SchemeGroupVersion.WithKind(RDSInstanceKind)
 )
 
-// RDSInstanceClass type metadata.
-var (
-	RDSInstanceClassKind             = reflect.TypeOf(RDSInstanceClass{}).Name()
-	RDSInstanceClassGroupKind        = schema.GroupKind{Group: Group, Kind: RDSInstanceClassKind}.String()
-	RDSInstanceClassKindAPIVersion   = RDSInstanceClassKind + "." + SchemeGroupVersion.String()
-	RDSInstanceClassGroupVersionKind = SchemeGroupVersion.WithKind(RDSInstanceClassKind)
-)
-
 // DBSubnetGroup type metadata.
 var (
 	DBSubnetGroupKind             = reflect.TypeOf(DBSubnetGroup{}).Name()
@@ -63,6 +55,5 @@ var (
 
 func init() {
 	SchemeBuilder.Register(&RDSInstance{}, &RDSInstanceList{})
-	SchemeBuilder.Register(&RDSInstanceClass{}, &RDSInstanceClassList{})
 	SchemeBuilder.Register(&DBSubnetGroup{}, &DBSubnetGroupList{})
 }
