97423325abc088395b7091dc653ea77d724d334f
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-09-30 14:30:30 +0300

    queue: use default initializers
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- pkg/controller/sqs/queue/controller.go
+++ pkg/controller/sqs/queue/controller.go
@@ -57,7 +57,6 @@ func SetupQueue(mgr ctrl.Manager, l logging.Logger) error {
 		Complete(managed.NewReconciler(mgr,
 			resource.ManagedKind(v1alpha1.QueueGroupVersionKind),
 			managed.WithExternalConnecter(&connector{kube: mgr.GetClient(), newClientFn: sqs.NewClient}),
-			managed.WithInitializers(managed.NewDefaultProviderConfig(mgr.GetClient())),
 			managed.WithLogger(l.WithValues("controller", name)),
 			managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
 }
