ba261a6e86e93a2ce2a9e02bc8eae8ebb2733bd4
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-11-05 16:13:44 +0300

    ack-generate: apigatewayv1 API has been generated

---
+++ apis/apigatewayv2/v1alpha1/zz_doc.go
@@ -0,0 +1,6 @@
+// +kubebuilder:object:generate=true
+// Package v1alpha1 is the v1alpha1 version of the apigatewayv2.aws.crossplane.io API.
+// +groupName=apigatewayv2.aws.crossplane.io
+// +versionName=v1alpha1
+
+package v1alpha1
