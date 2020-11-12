392edd799e386aa903cac1604dc016201c2a198a
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-11-12 18:26:52 +0300

    apigatewayv2.integration: implement custom hooks

--- pkg/controller/aws.go
+++ pkg/controller/aws.go
@@ -26,6 +26,7 @@ import (
 	"github.com/crossplane/provider-aws/pkg/controller/acmpca/certificateauthoritypermission"
 	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/api"
 	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/authorizer"
+	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/integration"
 	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/route"
 	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/stage"
 	"github.com/crossplane/provider-aws/pkg/controller/cache"
@@ -113,6 +114,7 @@ func Setup(mgr ctrl.Manager, l logging.Logger) error {
 		stage.SetupStage,
 		route.SetupRoute,
 		authorizer.SetupAuthorizer,
+		integration.SetupIntegration,
 	} {
 		if err := setup(mgr, l); err != nil {
 			return err
