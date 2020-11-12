0d0275422e9becc9c2e2923c66332bb4b6c924de
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-11-12 17:28:08 +0300

    ack resources: use all-lower names for controller packages

--- pkg/controller/apigatewayv2/integrationresponse/hooks.go
+++ pkg/controller/apigatewayv2/integrationresponse/hooks.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package integrationResponse
+package integrationresponse
 
 import (
 	"context"
