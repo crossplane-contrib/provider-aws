50d1c898789c0ff0d52ccfcf4362e3936cb711b0
hasheddan <georgedanielmangum@gmail.com>
2020-12-04 15:50:44 -0600

    Bump Provider apiVersion to v1beta1 in integration tests
    Crossplane recently dropped support for v1alpha1 pkg types. This updates
    the integration tests to be compatible.

--- cluster/local/integration_tests.sh
+++ cluster/local/integration_tests.sh
@@ -271,7 +271,7 @@ check_pods 2
 echo_step "installing ${PROJECT_NAME} into \"${CROSSPLANE_NAMESPACE}\" namespace"
 
 INSTALL_YAML="$( cat <<EOF
-apiVersion: pkg.crossplane.io/v1alpha1
+apiVersion: pkg.crossplane.io/v1beta1
 kind: Provider
 metadata:
   name: "${PACKAGE_NAME}"
