b6a84b9b1369e1964654cc56e829ca3c4c741205
Muvaffak Onus <onus.muvaffak@gmail.com>
2021-01-08 19:51:54 +0300

    lambda.function: generated by ACK
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

---
+++ apis/lambda/v1alpha1/zz_doc.go
@@ -0,0 +1,6 @@
+// +kubebuilder:object:generate=true
+// Package v1alpha1 is the v1alpha1 version of the lambda.aws.crossplane.io API.
+// +groupName=lambda.aws.crossplane.io
+// +versionName=v1alpha1
+
+package v1alpha1
