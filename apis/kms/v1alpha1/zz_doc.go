22857c21d1210f3c5dbf9b103042a41a63b775f9
hendrik.leppelsack <hendrik.leppelsack-extern@deutschebahn.com>
2021-02-10 17:57:39 +0100

    add KMS Key resource
    Signed-off-by: hendrik.leppelsack <hendrik.leppelsack-extern@deutschebahn.com>
    (external expert on behalf of DB Netz AG)

---
+++ apis/kms/v1alpha1/zz_doc.go
@@ -0,0 +1,6 @@
+// +kubebuilder:object:generate=true
+// Package v1alpha1 is the v1alpha1 version of the kms.aws.crossplane.io API.
+// +groupName=kms.aws.crossplane.io
+// +versionName=v1alpha1
+
+package v1alpha1
