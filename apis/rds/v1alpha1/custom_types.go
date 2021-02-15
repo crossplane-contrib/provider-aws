090f4b86ee64d0c957ad36df4e5deca8854ef284
Hitesh Ghia <hiteshghia@gmail.com>
2021-02-15 11:27:25 -0800

    Adding support for rds dbclustere
    Signed-off-by: Hitesh Ghia <hiteshghia@gmail.com>

---
+++ apis/rds/v1alpha1/custom_types.go
@@ -0,0 +1,102 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+
+// CustomDBClusterParameters are custom parameters for DBCluster
+type CustomDBClusterParameters struct {
+
+	// DomainIAMRoleNameRef is a reference to an IAMRole used to set
+	// DomainIAMRoleName.
+	// +optional
+	DomainIAMRoleNameRef *xpv1.Reference `json:"domainIAMRoleNameRef,omitempty"`
+
+	// DomainIAMRoleNameSelector selects a reference to an IAMRole used to set
+	// DomainIAMRoleName.
+	// +optional
+	DomainIAMRoleNameSelector *xpv1.Selector `json:"domainIAMRoleNameSelector,omitempty"`
+
+	// KMSKeyIDRef is a reference to a KMS Key used to set KMSKeyID.
+	// +optional
+	KMSKeyIDRef *xpv1.Reference `json:"kmsKeyIDRef,omitempty"`
+
+	// KMSKeyIDSelector selects a reference to a KMS Key used to set KMSKeyID.
+	// +optional
+	KMSKeyIDSelector *xpv1.Selector `json:"kmsKeyIDSelector,omitempty"`
+
+	// The password for the master database user. This password can contain any
+	// printable ASCII character except "/", """, or "@".
+	//
+	// Constraints: Must contain from 8 to 41 characters.
+	// +optional
+	MasterUserPasswordSecretRef *xpv1.SecretKeySelector `json:"masterUserPasswordSecretRef,omitempty"`
+
+	// A list of EC2 VPC security groups to associate with this DB cluster.
+	VPCSecurityGroupIDs []string `json:"vpcSecurityGroupIDs,omitempty"`
+
+	// VPCSecurityGroupIDRefs are references to VPCSecurityGroups used to set
+	// the VPCSecurityGroupIDs.
+	// +optional
+	VPCSecurityGroupIDRefs []xpv1.Reference `json:"vpcSecurityGroupIDRefs,omitempty"`
+
+	// VPCSecurityGroupIDSelector selects references to VPCSecurityGroups used
+	// to set the VPCSecurityGroupIDs.
+	// +optional
+	VPCSecurityGroupIDSelector *xpv1.Selector `json:"vpcSecurityGroupIDSelector,omitempty"`
+
+	// DBSubnetGroupNameRef is a reference to a DBSubnetGroup used to set
+	// DBSubnetGroupName.
+	// +immutable
+	// +optional
+	DBSubnetGroupNameRef *xpv1.Reference `json:"dbSubnetGroupNameRef,omitempty"`
+
+	// DBSubnetGroupNameSelector selects a reference to a DBSubnetGroup used to
+	// set DBSubnetGroupName.
+	// +immutable
+	// +optional
+	DBSubnetGroupNameSelector *xpv1.Selector `json:"dbSubnetGroupNameSelector,omitempty"`
+
+	// The DB cluster snapshot identifier of the new DB cluster snapshot created
+	// when SkipFinalSnapshot is disabled.
+	//
+	// Specifying this parameter and also skipping the creation of a final DB cluster
+	// snapshot with the SkipFinalShapshot parameter results in an error.
+	//
+	// Constraints:
+	//
+	//    * Must be 1 to 255 letters, numbers, or hyphens.
+	//
+	//    * First character must be a letter
+	//
+	//    * Can't end with a hyphen or contain two consecutive hyphens
+	// +immutable
+	// +optional
+	FinalDBSnapshotIdentifier string `json:"finalDBSnapshotIdentifier,omitempty"`
+
+	// A value that indicates whether to skip the creation of a final DB cluster
+	// snapshot before the DB cluster is deleted. If skip is specified, no DB cluster
+	// snapshot is created. If skip isn't specified, a DB cluster snapshot is created
+	// before the DB cluster is deleted. By default, skip isn't specified, and the
+	// DB cluster snapshot is created. By default, this parameter is disabled.
+	//
+	// You must specify a FinalDBSnapshotIdentifier parameter if SkipFinalSnapshot
+	// is disabled.
+	// +immutable
+	// +optional
+	SkipFinalSnapshot bool `json:"skipFinalSnapshot,omitempty"`
+}
---
+++ apis/rds/v1alpha1/referencers.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+
+	database "github.com/crossplane/provider-aws/apis/database/v1beta1"
+	network "github.com/crossplane/provider-aws/apis/ec2/v1beta1"
+	iamv1beta1 "github.com/crossplane/provider-aws/apis/identity/v1beta1"
+	kmsv1alpha1 "github.com/crossplane/provider-aws/apis/kms/v1alpha1"
+
+	"github.com/crossplane/crossplane-runtime/pkg/reference"
+	"github.com/pkg/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ResolveReferences of this DBCluster
+func (mg *DBCluster) ResolveReferences(ctx context.Context, c client.Reader) error {
+	r := reference.NewAPIResolver(c, mg)
+
+	// Resolve spec.forProvider.DomainIAMRoleName
+	rsp, err := r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.DomainIAMRoleName),
+		Reference:    mg.Spec.ForProvider.DomainIAMRoleNameRef,
+		Selector:     mg.Spec.ForProvider.DomainIAMRoleNameSelector,
+		To:           reference.To{Managed: &iamv1beta1.IAMRole{}, List: &iamv1beta1.IAMRoleList{}},
+		Extract:      reference.ExternalName(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "spec.forProvider.domainIAMRoleName")
+	}
+	mg.Spec.ForProvider.DomainIAMRoleName = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.DomainIAMRoleNameRef = rsp.ResolvedReference
+
+	// Resolve spec.forProvider.KMSKeyID
+	rsp, err = r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.KMSKeyID),
+		Reference:    mg.Spec.ForProvider.KMSKeyIDRef,
+		Selector:     mg.Spec.ForProvider.KMSKeyIDSelector,
+		To:           reference.To{Managed: &kmsv1alpha1.Key{}, List: &kmsv1alpha1.KeyList{}},
+		Extract:      reference.ExternalName(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "spec.forProvider.kmsKeyID")
+	}
+	mg.Spec.ForProvider.KMSKeyID = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.KMSKeyIDRef = rsp.ResolvedReference
+
+	// Resolve spec.forProvider.vpcSecurityGroupIds
+	mrsp, err := r.ResolveMultiple(ctx, reference.MultiResolutionRequest{
+		CurrentValues: mg.Spec.ForProvider.VPCSecurityGroupIDs,
+		References:    mg.Spec.ForProvider.VPCSecurityGroupIDRefs,
+		Selector:      mg.Spec.ForProvider.VPCSecurityGroupIDSelector,
+		To:            reference.To{Managed: &network.SecurityGroup{}, List: &network.SecurityGroupList{}},
+		Extract:       reference.ExternalName(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "spec.forProvider.vpcSecurityGroupIds")
+	}
+	mg.Spec.ForProvider.VPCSecurityGroupIDs = mrsp.ResolvedValues
+	mg.Spec.ForProvider.VPCSecurityGroupIDRefs = mrsp.ResolvedReferences
+
+	// Resolve spec.forProvider.dbSubnetGroupName
+	rsp, err = r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.DBSubnetGroupName),
+		Reference:    mg.Spec.ForProvider.DBSubnetGroupNameRef,
+		Selector:     mg.Spec.ForProvider.DBSubnetGroupNameSelector,
+		To:           reference.To{Managed: &database.DBSubnetGroup{}, List: &database.DBSubnetGroupList{}},
+		Extract:      reference.ExternalName(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "spec.forProvider.dbSubnetGroupName")
+	}
+	mg.Spec.ForProvider.DBSubnetGroupName = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.DBSubnetGroupNameRef = rsp.ResolvedReference
+
+	return nil
+}
