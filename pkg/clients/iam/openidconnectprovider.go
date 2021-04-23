502dcf06da067c375dc681f72bf152ed32d66809
Kellin McAvoy <kmcavoy@ea.com>
2021-04-23 14:40:56 -0500

    Add OpenIDConnectProvider resource
    Signed-off-by: Kellin McAvoy <kmcavoy@ea.com>

---
+++ pkg/clients/iam/openidconnectprovider.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package iam
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/crossplane/provider-aws/apis/identity/v1alpha1"
+	svcapitypes "github.com/crossplane/provider-aws/apis/identity/v1alpha1"
+)
+
+// NewOpenIDConnectProviderClient returns a new client using AWS credentials as JSON encoded data.
+func NewOpenIDConnectProviderClient(cfg aws.Config) OpenIDConnectProviderClient {
+	return iam.New(cfg)
+}
+
+// OpenIDConnectProviderClient is the external client used for IAM OpenIDConnectProvide Custom Resource
+type OpenIDConnectProviderClient interface {
+	GetOpenIDConnectProviderRequest(input *iam.GetOpenIDConnectProviderInput) iam.GetOpenIDConnectProviderRequest
+	CreateOpenIDConnectProviderRequest(input *iam.CreateOpenIDConnectProviderInput) iam.CreateOpenIDConnectProviderRequest
+	AddClientIDToOpenIDConnectProviderRequest(input *iam.AddClientIDToOpenIDConnectProviderInput) iam.AddClientIDToOpenIDConnectProviderRequest
+	RemoveClientIDFromOpenIDConnectProviderRequest(input *iam.RemoveClientIDFromOpenIDConnectProviderInput) iam.RemoveClientIDFromOpenIDConnectProviderRequest
+	UpdateOpenIDConnectProviderThumbprintRequest(input *iam.UpdateOpenIDConnectProviderThumbprintInput) iam.UpdateOpenIDConnectProviderThumbprintRequest
+	DeleteOpenIDConnectProviderRequest(input *iam.DeleteOpenIDConnectProviderInput) iam.DeleteOpenIDConnectProviderRequest
+}
+
+// GenerateOIDCProviderObservation is used to produce v1alpha1.OpenIDConnectProvider
+// from iam.OpenIDConnectProvider
+func GenerateOIDCProviderObservation(observed iam.GetOpenIDConnectProviderOutput) svcapitypes.OpenIDConnectProviderObservation {
+	o := v1alpha1.OpenIDConnectProviderObservation{}
+	if observed.CreateDate != nil {
+		createdTime := metav1.NewTime(*observed.CreateDate)
+		o.CreateDate = &createdTime
+	}
+	return o
+}
+
+// IsOIDCProviderUpToDate checks whether there is a change in any of the modifiable fields in OpenIDConnectProvider.
+func IsOIDCProviderUpToDate(in svcapitypes.OpenIDConnectProviderParameters, observed iam.GetOpenIDConnectProviderOutput) bool {
+	sortSlicesOpt := cmpopts.SortSlices(func(x, y string) bool {
+		return x < y
+	})
+	if !cmp.Equal(in.ClientIDList, observed.ClientIDList, sortSlicesOpt, cmpopts.EquateEmpty()) {
+		return false
+	}
+	if !cmp.Equal(in.ThumbprintList, observed.ThumbprintList, sortSlicesOpt, cmpopts.EquateEmpty()) {
+		return false
+	}
+	return true
+}
+
+// SliceDifference returns the elements to added and removed between the
+// current and desired slices
+func SliceDifference(current, desired []string) (add, remove []string) {
+	currentMap := make(map[string]struct{}, len(current))
+	for _, val := range current {
+		currentMap[val] = struct{}{}
+	}
+
+	for _, val := range desired {
+		_, exists := currentMap[val]
+		if !exists {
+			add = append(add, val)
+			continue
+		}
+		delete(currentMap, val)
+	}
+
+	for val := range currentMap {
+		remove = append(remove, val)
+	}
+	return
+}
