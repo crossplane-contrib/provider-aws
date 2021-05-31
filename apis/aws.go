1e6a512c783744576c56ff28c29c11a1f80d1a0e
qiffang <947321353@qq.com>
2021-05-31 14:46:31 +0800

    Remove unuse controller

--- apis/aws.go
+++ apis/aws.go
@@ -20,71 +20,54 @@ package apis
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	acmv1alpha1 "github.com/crossplane/provider-aws/apis/acm/v1alpha1"
-	acmpcav1alpha1 "github.com/crossplane/provider-aws/apis/acmpca/v1alpha1"
-	apigatewayv2 "github.com/crossplane/provider-aws/apis/apigatewayv2/v1alpha1"
-	cachev1alpha1 "github.com/crossplane/provider-aws/apis/cache/v1alpha1"
-	cachev1beta1 "github.com/crossplane/provider-aws/apis/cache/v1beta1"
-	databasev1beta1 "github.com/crossplane/provider-aws/apis/database/v1beta1"
-	dynamodbv1alpha1 "github.com/crossplane/provider-aws/apis/dynamodb/v1alpha1"
-	ec2v1alpha1 "github.com/crossplane/provider-aws/apis/ec2/v1alpha1"
-	ec2v1beta1 "github.com/crossplane/provider-aws/apis/ec2/v1beta1"
-	ecrv1alpha1 "github.com/crossplane/provider-aws/apis/ecr/v1alpha1"
-	efsv1alpha1 "github.com/crossplane/provider-aws/apis/efs/v1alpha1"
-	eksv1alpha1 "github.com/crossplane/provider-aws/apis/eks/v1alpha1"
-	eksv1beta1 "github.com/crossplane/provider-aws/apis/eks/v1beta1"
-	elasticloadbalancingv1alpha1 "github.com/crossplane/provider-aws/apis/elasticloadbalancing/v1alpha1"
-	identityv1alpha1 "github.com/crossplane/provider-aws/apis/identity/v1alpha1"
-	identityv1beta1 "github.com/crossplane/provider-aws/apis/identity/v1beta1"
 	kmsv1alpha1 "github.com/crossplane/provider-aws/apis/kms/v1alpha1"
-	lambdav1alpha1 "github.com/crossplane/provider-aws/apis/lambda/v1alpha1"
-	notificationv1alpha3 "github.com/crossplane/provider-aws/apis/notification/v1alpha1"
-	rdsv1alpha1 "github.com/crossplane/provider-aws/apis/rds/v1alpha1"
-	redshiftv1alpha1 "github.com/crossplane/provider-aws/apis/redshift/v1alpha1"
 	route53v1alpha1 "github.com/crossplane/provider-aws/apis/route53/v1alpha1"
 	s3v1alpha2 "github.com/crossplane/provider-aws/apis/s3/v1alpha3"
 	s3v1beta1 "github.com/crossplane/provider-aws/apis/s3/v1beta1"
-	secretsmanagerv1alpha1 "github.com/crossplane/provider-aws/apis/secretsmanager/v1alpha1"
-	servicediscoveryv1alpha1 "github.com/crossplane/provider-aws/apis/servicediscovery/v1alpha1"
-	sfnv1alpha1 "github.com/crossplane/provider-aws/apis/sfn/v1alpha1"
 	sqsv1beta1 "github.com/crossplane/provider-aws/apis/sqs/v1beta1"
 	awsv1alpha3 "github.com/crossplane/provider-aws/apis/v1alpha3"
 	awsv1beta1 "github.com/crossplane/provider-aws/apis/v1beta1"
+	ec2v1alpha1 "github.com/crossplane/provider-aws/apis/ec2/v1alpha1"
+	ec2v1beta1 "github.com/crossplane/provider-aws/apis/ec2/v1beta1"
+	elasticloadbalancingv1alpha1 "github.com/crossplane/provider-aws/apis/elasticloadbalancing/v1alpha1"
+	identityv1alpha1 "github.com/crossplane/provider-aws/apis/identity/v1alpha1"
+    identityv1beta1 "github.com/crossplane/provider-aws/apis/identity/v1beta1"
+
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
-		cachev1alpha1.SchemeBuilder.AddToScheme,
-		cachev1beta1.SchemeBuilder.AddToScheme,
-		databasev1beta1.SchemeBuilder.AddToScheme,
+		//cachev1alpha1.SchemeBuilder.AddToScheme,
+		//cachev1beta1.SchemeBuilder.AddToScheme,
+		//databasev1beta1.SchemeBuilder.AddToScheme,
 		elasticloadbalancingv1alpha1.SchemeBuilder.AddToScheme,
 		identityv1alpha1.SchemeBuilder.AddToScheme,
 		identityv1beta1.SchemeBuilder.AddToScheme,
 		route53v1alpha1.SchemeBuilder.AddToScheme,
-		notificationv1alpha3.SchemeBuilder.AddToScheme,
+		//notificationv1alpha3.SchemeBuilder.AddToScheme,
 		ec2v1beta1.SchemeBuilder.AddToScheme,
 		awsv1alpha3.SchemeBuilder.AddToScheme,
 		awsv1beta1.SchemeBuilder.AddToScheme,
-		acmv1alpha1.SchemeBuilder.AddToScheme,
+		//acmv1alpha1.SchemeBuilder.AddToScheme,
 		s3v1alpha2.SchemeBuilder.AddToScheme,
 		s3v1beta1.SchemeBuilder.AddToScheme,
-		secretsmanagerv1alpha1.SchemeBuilder.AddToScheme,
-		servicediscoveryv1alpha1.SchemeBuilder.AddToScheme,
-		acmpcav1alpha1.SchemeBuilder.AddToScheme,
-		eksv1beta1.SchemeBuilder.AddToScheme,
+		//secretsmanagerv1alpha1.SchemeBuilder.AddToScheme,
+		//servicediscoveryv1alpha1.SchemeBuilder.AddToScheme,
+		//acmpcav1alpha1.SchemeBuilder.AddToScheme,
+		//eksv1beta1.SchemeBuilder.AddToScheme,
 		sqsv1beta1.SchemeBuilder.AddToScheme,
-		redshiftv1alpha1.SchemeBuilder.AddToScheme,
-		eksv1alpha1.SchemeBuilder.AddToScheme,
-		ecrv1alpha1.SchemeBuilder.AddToScheme,
-		apigatewayv2.SchemeBuilder.AddToScheme,
-		sfnv1alpha1.SchemeBuilder.AddToScheme,
-		dynamodbv1alpha1.SchemeBuilder.AddToScheme,
+		//redshiftv1alpha1.SchemeBuilder.AddToScheme,
+		//eksv1alpha1.SchemeBuilder.AddToScheme,
+		//ecrv1alpha1.SchemeBuilder.AddToScheme,
+		//apigatewayv2.SchemeBuilder.AddToScheme,
+		//sfnv1alpha1.SchemeBuilder.AddToScheme,
+		//dynamodbv1alpha1.SchemeBuilder.AddToScheme,
 		kmsv1alpha1.SchemeBuilder.AddToScheme,
-		efsv1alpha1.SchemeBuilder.AddToScheme,
-		rdsv1alpha1.SchemeBuilder.AddToScheme,
+		//efsv1alpha1.SchemeBuilder.AddToScheme,
+		//rdsv1alpha1.SchemeBuilder.AddToScheme,
 		ec2v1alpha1.SchemeBuilder.AddToScheme,
-		lambdav1alpha1.SchemeBuilder.AddToScheme,
+		//lambdav1alpha1.SchemeBuilder.AddToScheme,
 	)
 }
 
--- pkg/controller/aws.go
+++ pkg/controller/aws.go
@@ -22,74 +22,18 @@ import (
 
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
 
-	"github.com/crossplane/provider-aws/pkg/controller/acm"
-	"github.com/crossplane/provider-aws/pkg/controller/acmpca/certificateauthority"
-	"github.com/crossplane/provider-aws/pkg/controller/acmpca/certificateauthoritypermission"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/api"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/apimapping"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/authorizer"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/deployment"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/domainname"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/integration"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/integrationresponse"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/model"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/route"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/routeresponse"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/stage"
-	"github.com/crossplane/provider-aws/pkg/controller/apigatewayv2/vpclink"
-	"github.com/crossplane/provider-aws/pkg/controller/cache"
-	"github.com/crossplane/provider-aws/pkg/controller/cache/cachesubnetgroup"
-	"github.com/crossplane/provider-aws/pkg/controller/cache/cluster"
 	"github.com/crossplane/provider-aws/pkg/controller/config"
-	"github.com/crossplane/provider-aws/pkg/controller/database"
-	"github.com/crossplane/provider-aws/pkg/controller/database/dbsubnetgroup"
-	"github.com/crossplane/provider-aws/pkg/controller/dynamodb/backup"
-	"github.com/crossplane/provider-aws/pkg/controller/dynamodb/globaltable"
-	"github.com/crossplane/provider-aws/pkg/controller/dynamodb/table"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/address"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/internetgateway"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/natgateway"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/routetable"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/securitygroup"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/subnet"
 	"github.com/crossplane/provider-aws/pkg/controller/ec2/vpc"
-	"github.com/crossplane/provider-aws/pkg/controller/ec2/vpccidrblock"
-	"github.com/crossplane/provider-aws/pkg/controller/ecr/repository"
-	"github.com/crossplane/provider-aws/pkg/controller/ecr/repositorypolicy"
-	"github.com/crossplane/provider-aws/pkg/controller/efs/filesystem"
-	"github.com/crossplane/provider-aws/pkg/controller/eks"
-	"github.com/crossplane/provider-aws/pkg/controller/eks/fargateprofile"
-	"github.com/crossplane/provider-aws/pkg/controller/eks/nodegroup"
 	"github.com/crossplane/provider-aws/pkg/controller/elasticloadbalancing/elb"
 	"github.com/crossplane/provider-aws/pkg/controller/elasticloadbalancing/elbattachment"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/iamaccesskey"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/iamgroup"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/iamgrouppolicyattachment"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/iamgroupusermembership"
 	"github.com/crossplane/provider-aws/pkg/controller/identity/iampolicy"
 	"github.com/crossplane/provider-aws/pkg/controller/identity/iamrole"
 	"github.com/crossplane/provider-aws/pkg/controller/identity/iamrolepolicyattachment"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/iamuser"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/iamuserpolicyattachment"
-	"github.com/crossplane/provider-aws/pkg/controller/identity/openidconnectprovider"
 	"github.com/crossplane/provider-aws/pkg/controller/kms/key"
-	"github.com/crossplane/provider-aws/pkg/controller/lambda/function"
-	"github.com/crossplane/provider-aws/pkg/controller/notification/snssubscription"
-	"github.com/crossplane/provider-aws/pkg/controller/notification/snstopic"
-	"github.com/crossplane/provider-aws/pkg/controller/rds/dbcluster"
-	"github.com/crossplane/provider-aws/pkg/controller/rds/dbparametergroup"
-	"github.com/crossplane/provider-aws/pkg/controller/rds/globalcluster"
-	"github.com/crossplane/provider-aws/pkg/controller/redshift"
 	"github.com/crossplane/provider-aws/pkg/controller/route53/hostedzone"
 	"github.com/crossplane/provider-aws/pkg/controller/route53/resourcerecordset"
 	"github.com/crossplane/provider-aws/pkg/controller/s3"
 	"github.com/crossplane/provider-aws/pkg/controller/s3/bucketpolicy"
-	"github.com/crossplane/provider-aws/pkg/controller/secretsmanager/secret"
-	"github.com/crossplane/provider-aws/pkg/controller/servicediscovery/httpnamespace"
-	"github.com/crossplane/provider-aws/pkg/controller/servicediscovery/privatednsnamespace"
-	"github.com/crossplane/provider-aws/pkg/controller/servicediscovery/publicdnsnamespace"
-	"github.com/crossplane/provider-aws/pkg/controller/sfn/activity"
-	"github.com/crossplane/provider-aws/pkg/controller/sfn/statemachine"
 	"github.com/crossplane/provider-aws/pkg/controller/sqs/queue"
 )
 
@@ -98,74 +42,74 @@ import (
 func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter) error {
 	for _, setup := range []func(ctrl.Manager, logging.Logger, workqueue.RateLimiter) error{
 		config.Setup,
-		cache.SetupReplicationGroup,
-		cachesubnetgroup.SetupCacheSubnetGroup,
-		cluster.SetupCacheCluster,
-		database.SetupRDSInstance,
-		eks.SetupCluster,
+		//cache.SetupReplicationGroup,
+		//cachesubnetgroup.SetupCacheSubnetGroup,
+		//cluster.SetupCacheCluster,
+		//database.SetupRDSInstance,
+		//eks.SetupCluster,
 		elb.SetupELB,
 		elbattachment.SetupELBAttachment,
-		nodegroup.SetupNodeGroup,
+		//nodegroup.SetupNodeGroup,
 		s3.SetupBucket,
 		bucketpolicy.SetupBucketPolicy,
-		iamaccesskey.SetupIAMAccessKey,
-		iamuser.SetupIAMUser,
-		iamgroup.SetupIAMGroup,
+		//iamaccesskey.SetupIAMAccessKey,
+		//iamuser.SetupIAMUser,
+		//iamgroup.SetupIAMGroup,
 		iampolicy.SetupIAMPolicy,
 		iamrole.SetupIAMRole,
-		iamgroupusermembership.SetupIAMGroupUserMembership,
-		iamuserpolicyattachment.SetupIAMUserPolicyAttachment,
-		iamgrouppolicyattachment.SetupIAMGroupPolicyAttachment,
+		//iamgroupusermembership.SetupIAMGroupUserMembership,
+		//iamuserpolicyattachment.SetupIAMUserPolicyAttachment,
+		//iamgrouppolicyattachment.SetupIAMGroupPolicyAttachment,
 		iamrolepolicyattachment.SetupIAMRolePolicyAttachment,
 		vpc.SetupVPC,
-		subnet.SetupSubnet,
-		securitygroup.SetupSecurityGroup,
-		internetgateway.SetupInternetGateway,
-		natgateway.SetupNatGateway,
-		routetable.SetupRouteTable,
-		dbsubnetgroup.SetupDBSubnetGroup,
-		certificateauthority.SetupCertificateAuthority,
-		certificateauthoritypermission.SetupCertificateAuthorityPermission,
-		acm.SetupCertificate,
+		//subnet.SetupSubnet,
+		//securitygroup.SetupSecurityGroup,
+		//internetgateway.SetupInternetGateway,
+		//natgateway.SetupNatGateway,
+		//routetable.SetupRouteTable,
+		//dbsubnetgroup.SetupDBSubnetGroup,
+		//certificateauthority.SetupCertificateAuthority,
+		//certificateauthoritypermission.SetupCertificateAuthorityPermission,
+		//acm.SetupCertificate,
 		resourcerecordset.SetupResourceRecordSet,
 		hostedzone.SetupHostedZone,
-		secret.SetupSecret,
-		snstopic.SetupSNSTopic,
-		snssubscription.SetupSubscription,
+		//secret.SetupSecret,
+		//snstopic.SetupSNSTopic,
+		//snssubscription.SetupSubscription,
 		queue.SetupQueue,
-		redshift.SetupCluster,
-		address.SetupAddress,
-		repository.SetupRepository,
-		repositorypolicy.SetupRepositoryPolicy,
-		api.SetupAPI,
-		stage.SetupStage,
-		route.SetupRoute,
-		authorizer.SetupAuthorizer,
-		integration.SetupIntegration,
-		deployment.SetupDeployment,
-		domainname.SetupDomainName,
-		integrationresponse.SetupIntegrationResponse,
-		model.SetupModel,
-		apimapping.SetupAPIMapping,
-		routeresponse.SetupRouteResponse,
-		vpclink.SetupVPCLink,
-		fargateprofile.SetupFargateProfile,
-		activity.SetupActivity,
-		statemachine.SetupStateMachine,
-		table.SetupTable,
-		backup.SetupBackup,
-		globaltable.SetupGlobalTable,
+		//redshift.SetupCluster,
+		//address.SetupAddress,
+		//repository.SetupRepository,
+		//repositorypolicy.SetupRepositoryPolicy,
+		//api.SetupAPI,
+		//stage.SetupStage,
+		//route.SetupRoute,
+		//authorizer.SetupAuthorizer,
+		//integration.SetupIntegration,
+		//deployment.SetupDeployment,
+		//domainname.SetupDomainName,
+		//integrationresponse.SetupIntegrationResponse,
+		//model.SetupModel,
+		//apimapping.SetupAPIMapping,
+		//routeresponse.SetupRouteResponse,
+		//vpclink.SetupVPCLink,
+		//fargateprofile.SetupFargateProfile,
+		//activity.SetupActivity,
+		//statemachine.SetupStateMachine,
+		//table.SetupTable,
+		//backup.SetupBackup,
+		//globaltable.SetupGlobalTable,
 		key.SetupKey,
-		filesystem.SetupFileSystem,
-		dbcluster.SetupDBCluster,
-		dbparametergroup.SetupDBParameterGroup,
-		globalcluster.SetupGlobalCluster,
-		vpccidrblock.SetupVPCCIDRBlock,
-		privatednsnamespace.SetupPrivateDNSNamespace,
-		publicdnsnamespace.SetupPublicDNSNamespace,
-		httpnamespace.SetupHTTPNamespace,
-		function.SetupFunction,
-		openidconnectprovider.SetupOpenIDConnectProvider,
+		//filesystem.SetupFileSystem,
+		//dbcluster.SetupDBCluster,
+		//dbparametergroup.SetupDBParameterGroup,
+		//globalcluster.SetupGlobalCluster,
+		//vpccidrblock.SetupVPCCIDRBlock,
+		//privatednsnamespace.SetupPrivateDNSNamespace,
+		//publicdnsnamespace.SetupPublicDNSNamespace,
+		//httpnamespace.SetupHTTPNamespace,
+		//function.SetupFunction,
+		//openidconnectprovider.SetupOpenIDConnectProvider,
 	} {
 		if err := setup(mgr, l, rl); err != nil {
 			return err
