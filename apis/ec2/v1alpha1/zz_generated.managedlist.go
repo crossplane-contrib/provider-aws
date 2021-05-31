f8dfe2938c7e4cc2ad13fea0fdcda97738565b53
Dkaykay <denis.holschuh@googlemail.com>
2021-05-31 08:47:05 +0200

    feat: merge deepcopy and other generated functions
    Signed-off-by: Dkaykay <denis.holschuh@googlemail.com>

--- apis/ec2/v1alpha1/zz_generated.managedlist.go
+++ apis/ec2/v1alpha1/zz_generated.managedlist.go
@@ -20,8 +20,8 @@ package v1alpha1
 
 import resource "github.com/crossplane/crossplane-runtime/pkg/resource"
 
-// GetItems of this VPCPeeringConnectionList.
-func (l *VPCPeeringConnectionList) GetItems() []resource.Managed {
+// GetItems of this VPCCIDRBlockList.
+func (l *VPCCIDRBlockList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
 	for i := range l.Items {
 		items[i] = &l.Items[i]
@@ -29,8 +29,8 @@ func (l *VPCPeeringConnectionList) GetItems() []resource.Managed {
 	return items
 }
 
-// GetItems of this VPCCIDRBlockList.
-func (l *VPCCIDRBlockList) GetItems() []resource.Managed {
+// GetItems of this VPCPeeringConnectionList.
+func (l *VPCPeeringConnectionList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
 	for i := range l.Items {
 		items[i] = &l.Items[i]
