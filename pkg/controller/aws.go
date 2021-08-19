62665478464aaceb1688d41ac7923bf5cf85d16c
Darryl Sw <darryl_sw@hive.gov.sg>
2021-08-19 15:14:09 +0800

    feat: implement VPC Endpoint for AWS Provider
    Signed-off-by: Darryl Sw <darryl_sw@hive.gov.sg>

--- pkg/controller/aws.go
+++ pkg/controller/aws.go
@@ -70,6 +70,7 @@ import (
 	"github.com/crossplane/provider-aws/pkg/controller/ec2/volume"
 	"github.com/crossplane/provider-aws/pkg/controller/ec2/vpc"
 	"github.com/crossplane/provider-aws/pkg/controller/ec2/vpccidrblock"
+	"github.com/crossplane/provider-aws/pkg/controller/ec2/vpcendpoint"
 	"github.com/crossplane/provider-aws/pkg/controller/ec2/vpcpeeringconnection"
 	"github.com/crossplane/provider-aws/pkg/controller/ecr/repository"
 	"github.com/crossplane/provider-aws/pkg/controller/ecr/repositorypolicy"
@@ -220,6 +221,7 @@ func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter, poll ti
 		resolverendpoint.SetupResolverEndpoint,
 		resolverrule.SetupResolverRule,
 		vpcpeeringconnection.SetupVPCPeeringConnection,
+		vpcendpoint.SetupVPCEndpoint,
 		kafkacluster.SetupCluster,
 		efsmounttarget.SetupMountTarget,
 		transferserver.SetupServer,
