c5be090848d627af3162c4c917932c266fa03035
Benedikt Bock <benedikt.bock@accenture.com>
2022-02-01 19:15:41 +0100

    feat(cognito): Add IdentityPool resource
    Signed-off-by: Benedikt Bock <benedikt.bock-extern@deutschebahn.com>
    (external expert on behalf of DB Netz AG)

--- apis/aws.go
+++ apis/aws.go
@@ -33,6 +33,7 @@ import (
 	cloudfrontv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/cloudfront/v1alpha1"
 	cloudsearchv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/cloudsearch/v1alpha1"
 	cloudwatchlogsv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/cloudwatchlogs/v1alpha1"
+	cognitoidentityv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/cognitoidentity/v1alpha1"
 	cognitoidentityproviderv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/cognitoidentityprovider/v1alpha1"
 	databasev1beta1 "github.com/crossplane-contrib/provider-aws/apis/database/v1beta1"
 	daxv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/dax/v1alpha1"
@@ -151,6 +152,7 @@ func init() {
 		prometheusservice.SchemeBuilder.AddToScheme,
 		cloudsearchv1alpha1.AddToScheme,
 		apigatewayv1alpha1.AddToScheme,
+		cognitoidentityv1alpha1.AddToScheme,
 	)
 }
 
