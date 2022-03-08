e572d2cf3a0bcace4aef1c55471fbe673c60786e
Carl Henrik Lunde <chlunde@ifi.uio.no>
2022-03-08 23:53:57 +0100

    rds: Fix DBInstanceRoleAssociation deletion
    Fix the IsNotFound handler so we can delete the RDS instance before the
    role.
    
    Signed-off-by: Carl Henrik Lunde <chlunde@ifi.uio.no>

--- pkg/controller/rds/dbinstanceroleassociation/zz_conversions.go
+++ pkg/controller/rds/dbinstanceroleassociation/zz_conversions.go
@@ -53,5 +53,5 @@ func GenerateRemoveRoleFromDBInstanceInput(cr *svcapitypes.DBInstanceRoleAssocia
 // IsNotFound returns whether the given error is of type NotFound or not.
 func IsNotFound(err error) bool {
 	awsErr, ok := err.(awserr.Error)
-	return ok && awsErr.Code() == "UNKNOWN"
+	return ok && awsErr.Code() == "DBInstanceNotFound"
 }
