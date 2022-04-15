f5c76f04a42e8967d316b56ba723025a82c671e6
haarchri <chhaar30@googlemail.com>
2022-04-15 13:15:40 +0200

    fix(efs): generate latest master stuff for efs access point
    Signed-off-by: haarchri <chhaar30@googlemail.com>

--- apis/efs/v1alpha1/zz_generated.managed.go
+++ apis/efs/v1alpha1/zz_generated.managed.go
@@ -43,6 +43,11 @@ func (mg *AccessPoint) GetProviderReference() *xpv1.Reference {
 	return mg.Spec.ProviderReference
 }
 
+// GetPublishConnectionDetailsTo of this AccessPoint.
+func (mg *AccessPoint) GetPublishConnectionDetailsTo() *xpv1.PublishConnectionDetailsTo {
+	return mg.Spec.PublishConnectionDetailsTo
+}
+
 // GetWriteConnectionSecretToReference of this AccessPoint.
 func (mg *AccessPoint) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
 	return mg.Spec.WriteConnectionSecretToReference
@@ -69,6 +74,11 @@ Deprecated: Use SetProviderConfigReference.
 */
 func (mg *AccessPoint) SetProviderReference(r *xpv1.Reference) {
 	mg.Spec.ProviderReference = r
+}
+
+// SetPublishConnectionDetailsTo of this AccessPoint.
+func (mg *AccessPoint) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo) {
+	mg.Spec.PublishConnectionDetailsTo = r
 }
 
 // SetWriteConnectionSecretToReference of this AccessPoint.
