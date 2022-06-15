fd6df065e059b44aae21f0b49ed3bf704bc39fb2
Diego Pinheiro <diego.milhomes@gmail.com>
2022-06-15 18:47:00 +0000

    Update description for StoreConfig
    Signed-off-by: GitHub <noreply@github.com>

--- apis/v1alpha1/storeconfig_types.go
+++ apis/v1alpha1/storeconfig_types.go
@@ -34,11 +34,11 @@ type StoreConfigStatus struct {
 
 // +kubebuilder:object:root=true
 
-// A StoreConfig configures how GCP controller should store connection details.
+// A StoreConfig configures how AWS controller should store connection details.
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="TYPE",type="string",JSONPath=".spec.type"
 // +kubebuilder:printcolumn:name="DEFAULT-SCOPE",type="string",JSONPath=".spec.defaultScope"
-// +kubebuilder:resource:scope=Cluster,categories={crossplane,store,gcp}
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,store,aws}
 // +kubebuilder:subresource:status
 type StoreConfig struct {
 	metav1.TypeMeta   `json:",inline"`
