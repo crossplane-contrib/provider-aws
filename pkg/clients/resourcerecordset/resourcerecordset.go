56fe141458a416606e8b8b35259203b14dca66cb
Manabu Mccloskey <manabu.mccloskey@gmail.com>
2022-06-23 18:57:18 -0600

    fix route53 wildcard handling
    Signed-off-by: Manabu Mccloskey <manabu.mccloskey@gmail.com>

--- pkg/clients/resourcerecordset/resourcerecordset.go
+++ pkg/clients/resourcerecordset/resourcerecordset.go
@@ -39,6 +39,7 @@ import (
 
 const (
 	errResourceRecordSetNotFound = "ResourceRecordSet.NotFound"
+	wildCardCharacters           = "\\052"
 )
 
 // Client defines ResourceRecordSet operations
@@ -84,8 +85,14 @@ func GetResourceRecordSet(ctx context.Context, name string, params v1alpha1.Reso
 		}
 		return s
 	}
+	replaceWithWildCard := func(s string) string {
+		if strings.HasPrefix(s, wildCardCharacters) {
+			return strings.Replace(s, wildCardCharacters, "*", 1)
+		}
+		return s
+	}
 	for _, rr := range res.ResourceRecordSets {
-		if appendDot(aws.ToString(rr.Name)) == appendDot(name) &&
+		if replaceWithWildCard(appendDot(aws.ToString(rr.Name))) == appendDot(name) &&
 			string(rr.Type) == params.Type &&
 			aws.ToString(rr.SetIdentifier) == aws.ToString(params.SetIdentifier) {
 			return &rr, nil
