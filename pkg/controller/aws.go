b8c328748da7f75bb4c83db29ccebccf68d28cae
Tiago Posse <tiago.posse@hi.health>
2022-07-29 16:02:15 +0200

    introduce method resource, move all examples of apigateway to use example as name
    Signed-off-by: Tiago Posse <tiago.posse@hi.health>
    Signed-off-by: Alessandro Benoni <alessandro@bredec.com>

--- pkg/controller/aws.go
+++ pkg/controller/aws.go
@@ -24,6 +24,7 @@ import (
 	"github.com/crossplane-contrib/provider-aws/pkg/controller/acm"
 	"github.com/crossplane-contrib/provider-aws/pkg/controller/acmpca/certificateauthority"
 	"github.com/crossplane-contrib/provider-aws/pkg/controller/acmpca/certificateauthoritypermission"
+	"github.com/crossplane-contrib/provider-aws/pkg/controller/apigateway/method"
 	"github.com/crossplane-contrib/provider-aws/pkg/controller/apigateway/resource"
 	"github.com/crossplane-contrib/provider-aws/pkg/controller/apigateway/restapi"
 	"github.com/crossplane-contrib/provider-aws/pkg/controller/apigatewayv2/api"
@@ -325,6 +326,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 		prometheusservicealertmanagerdefinition.SetupAlertManagerDefinition,
 		resource.SetupResource,
 		restapi.SetupRestAPI,
+		method.SetupMethod,
 		cognitoidentitypool.SetupIdentityPool,
 	} {
 		if err := setup(mgr, o); err != nil {
