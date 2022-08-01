2ed46c80cf0b60f208ca6834fdf63d20fe124874
André Kesser <andre@kesser.de>
2022-08-01 07:33:05 +0200

    feat: Added types and generated crd for securitygrouprule
    Signed-off-by: André Kesser <andre.kesser@dkb.de>
    Signed-off-by: Alessandro Benoni <alessandro@bredec.com>

--- apis/ec2/manualv1alpha1/zz_generated.managedlist.go
+++ apis/ec2/manualv1alpha1/zz_generated.managedlist.go
@@ -29,6 +29,15 @@ func (l *InstanceList) GetItems() []resource.Managed {
 	return items
 }
 
+// GetItems of this SecurityGroupRuleList.
+func (l *SecurityGroupRuleList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this VPCCIDRBlockList.
 func (l *VPCCIDRBlockList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
