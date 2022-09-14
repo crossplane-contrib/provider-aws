b78d55e44bd8024d2941f550312d43c238c2b9f1
Steven Borrelli <steve@borrelli.org>
2022-09-14 18:34:52 -0500

    fix import linting issue
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- apis/apigatewayv2/v1alpha1/referencers.go
+++ apis/apigatewayv2/v1alpha1/referencers.go
@@ -21,9 +21,9 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	ec2 "github.com/crossplane-contrib/provider-aws/apis/ec2/v1beta1"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/crossplane/crossplane-runtime/pkg/reference"
 	resource "github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/pkg/errors"
