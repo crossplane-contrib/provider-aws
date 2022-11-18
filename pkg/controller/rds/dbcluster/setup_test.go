8466fae746b4627e6046feb30bd5d6510788332d
Paul Schroeder <97968850+schroeder-paul@users.noreply.github.com>
2022-11-18 13:43:42 +0100

    fix(rds): DBCluster controller not recognizing changes in VpcSecurityGroups
    Signed-off-by: Paul Schroeder <97968850+schroeder-paul@users.noreply.github.com>

---
+++ pkg/controller/rds/dbcluster/setup_test.go
@@ -0,0 +1,188 @@
+package dbcluster
+
+import (
+	"testing"
+
+	svcsdk "github.com/aws/aws-sdk-go/service/rds"
+
+	svcapitypes "github.com/crossplane-contrib/provider-aws/apis/rds/v1alpha1"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func ptr(str string) *string {
+	return &str
+}
+
+func TestIsVPCSecurityGroupIDsUpToDate(t *testing.T) {
+	type args struct {
+		cr  *svcapitypes.DBCluster
+		out *svcsdk.DescribeDBClustersOutput
+	}
+
+	type want struct {
+		isUpToDate bool
+	}
+
+	cases := map[string]struct {
+		args
+		want
+	}{
+		"NotAsMany": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-789"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: false,
+			},
+		},
+		"DesiredEmpty": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: nil,
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: false,
+			},
+		},
+		"ActualEmpty": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: nil,
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: false,
+			},
+		},
+		"Unsorted": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: true,
+			},
+		},
+		"Identical": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: true,
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			isUpToDate := isVPCSecurityGroupIDsUpToDate(tc.args.cr, tc.args.out)
+
+			if diff := cmp.Diff(tc.want.isUpToDate, isUpToDate); diff != "" {
+				t.Errorf("r: -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
