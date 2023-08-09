f494496cd72bb3928d2d5de203f54888e8cd6192
Philipp Hinteregger <philipp.hinteregger@dynatrace.com>
2023-08-09 14:59:53 +0200

    feat(capacityreservation): add postObserve function to capasitireservation controller setup
    Signed-off-by: Philipp Hinteregger <philipp.hinteregger@dynatrace.com>

--- pkg/controller/ec2/capacityreservation/setup.go
+++ pkg/controller/ec2/capacityreservation/setup.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	svcsdk "github.com/aws/aws-sdk-go/service/ec2"
 	svcapitypes "github.com/crossplane-contrib/provider-aws/apis/ec2/v1alpha1"
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	"github.com/crossplane-contrib/provider-aws/apis/v1alpha1"
 	aws "github.com/crossplane-contrib/provider-aws/pkg/clients"
 	"github.com/crossplane-contrib/provider-aws/pkg/features"
@@ -43,6 +44,25 @@ func postCreate(_ context.Context, cr *svcapitypes.CapacityReservation, resp *sv
 	if err != nil {
 		return managed.ExternalCreation{}, err
 	}
-	meta.SetExternalName(cr, aws.StringValue(resp.ApiId))
+	meta.SetExternalName(cr, aws.StringValue(resp.CapacityReservation.CapacityReservationArn))
 	return cre, nil
+}
+
+func postObserve(_ context.Context, cr *svcapitypes.CapacityReservation, resp *svcsdk.DescribeCapacityReservationsOutput, obs managed.ExternalObservation, err error) (managed.ExternalObservation, error) {
+	if err != nil {
+		return managed.ExternalObservation{}, err
+	}
+	switch(*resp.CapacityReservations[0].State) {
+	case string("active"):
+		cr.SetConditions(xpv1.Available())
+	case string("expired"):
+		cr.SetConditions(xpv1.Unavailable())
+	case string("cancelled"):
+		cr.SetConditions(xpv1.Unavailable())
+	case string("pending"):
+		cr.SetConditions(xpv1.Creating())
+	case string("failed"):
+		cr.SetConditions(xpv1.Unavailable())
+	}
+	return obs, nil
 }
