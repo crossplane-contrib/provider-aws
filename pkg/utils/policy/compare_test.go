bab20dd86d51b2d0a27ab73f0f3c049a4ddf47ee
Maximilian Blatt (external expert on behalf of DB Netz) <maximilian.blatt-extern@deutschebahn.com>
2023-10-10 17:33:24 +0200

    fix(kms): Parse key policies before compare
    Signed-off-by: Maximilian Blatt (external expert on behalf of DB Netz) <maximilian.blatt-extern@deutschebahn.com>
    (cherry picked from commit c11fb02f73e2c8151f7ee7a361f5fc3f92a97d5a)

---
+++ pkg/utils/policy/compare_test.go
@@ -0,0 +1,73 @@
+package policy
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var (
+	//go:embed testdata/Issue1892_a.json
+	policyIssue1892a string
+
+	//go:embed testdata/Issue1892_a_min.json
+	policyIssue1892aMin string
+
+	//go:embed testdata/Issue1892_b.json
+	policyIssue1892b string
+
+	//go:embed testdata/Issue1892_b_min.json
+	policyIssue1892bMin string
+)
+
+func TestCompareRawPolicies(t *testing.T) {
+	type args struct {
+		policyA string
+		policyB string
+	}
+	type want struct {
+		equals bool
+	}
+	cases := map[string]struct {
+		want
+		args
+	}{
+		"Issue1892_a": {
+			args: args{
+				policyA: policyIssue1892a,
+				policyB: policyIssue1892aMin,
+			},
+			want: want{
+				equals: true,
+			},
+		},
+		"Issue1892_b": {
+			args: args{
+				policyA: policyIssue1892b,
+				policyB: policyIssue1892bMin,
+			},
+			want: want{
+				equals: true,
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			pA, err := ParsePolicyString(tc.args.policyA)
+			if err != nil {
+				t.Fatal(err)
+			}
+			pB, err := ParsePolicyString(tc.args.policyB)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			equal, pDiff := ArePoliciesEqal(&pA, &pB)
+			if diff := cmp.Diff(&tc.want.equals, &equal); diff != "" {
+				t.Errorf("ArePoliciesEqal(...): -want, +got\n:%s\nDiff: -want +got\n:%s", diff, pDiff)
+			}
+		})
+	}
+}
