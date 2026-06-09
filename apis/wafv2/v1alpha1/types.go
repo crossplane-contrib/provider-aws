e7ce8b296ed871933605ec7304dd829b457814de
Jorge Millán <jbcjorge@gmail.com>
2026-06-09 13:29:19 +0200

    feat(wafv2): Add IPSet controller
    This adds the WAFv2:IPSet managed resource. The implementation is manual
    (not ACK-generated) following the same approach as the existing WebACL
    controller.
    
    The controller supports full CRUD lifecycle with optimistic locking via
    LockToken, tag reconciliation, and order-independent address comparison.
    
    Signed-off-by: Jorge Millán <jbcjorge@gmail.com>

--- apis/wafv2/v1alpha1/types.go
+++ apis/wafv2/v1alpha1/types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 The Crossplane Authors.
+Copyright 2026 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -557,7 +557,7 @@ type Headers struct {
 }
 
 // +kubebuilder:skipversion
-type IPSet struct {
+type IPSet_SDK struct {
 	ARN *string `json:"arn,omitempty"`
 
 	Description *string `json:"description,omitempty"`
